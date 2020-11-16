f1321397770b8489d68c78ef9480875058b55283
고서연 <seoyeon@goseoyeon-ui-MacBookPro.local>
2020-11-16 15:43:51 +0900

    Chap06 | 컴포넌트 반복

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, {Component} from 'react';
-import ScrollBox from './ScrollBox.js';
+import IterationSample from './IterationSample.js';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.scorllToBottom()}>맨밑으로</button>
-      </div>
+      <IterationSample/>
     );
   }
 }
