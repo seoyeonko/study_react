b592c3fb1db4c2d71a5a232252b4e314ebdfe9a1
고서연 <seoyeon@goseoyeon-ui-MacBookPro.local>
2020-11-16 19:43:58 +0900

    201116 study review (게시판)

--- src/review/Board.js
+++ src/review/Board.js
@@ -2,22 +2,43 @@ import React, {useState} from 'react';
 
 const Board= () => {
 
-    // map 함수 사용 -> 5, 6장에서 배움
-    // map 함수 빼고 완성해보기!
-    const [lists, setLists] = useState([
-        { 
-            id: 1,
-            title: "첫 번째 글",
-            contents: "React 첫 Study",
-        },
-        {
-            id: 2,
-            title:" 두 번째 글",
-            contents: "아자아자!!",
-        }
+    const [boards, setBoard] = useState([
+        {id:1, title:'첫번째 글', contents: 'First React Study'},
+        {id:2, title:'두번째 글', contents: 'Second React Study'},
     ]);
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputContent, setInputContent] = useState('');
+    const [nextId, setNextId] = useState(3);
 
-    // const onClickWrite = () => setLists({id})
+    
+    const onChange1 = e => setInputTitle(e.target.value);
+    const onChange2 = e => setInputContent(e.target.value);
+    const onClick = () => {
+        const nextBoards = boards.concat({
+            id: nextId,
+            title: inputTitle,
+            contents: inputContent
+        });
+        setNextId(nextId+1);
+        setBoard(nextBoards);
+        setInputTitle('');
+        setInputContent('');
+    }
+    const onRemove = id => {
+        const nextBoards = boards.filter(
+            board => board.id !== id
+        );
+        setBoard(nextBoards);
+    }
+    
+
+    const boardList = boards.map(board => 
+        <div key={board.id} onDoubleClick={()=>onRemove(board.id)}>
+            <h3>글 제목 : {board.title}</h3>
+            <p>글 내용: {board.contents}</p>
+        </div>
+    )
+
 
     return (
         <div>
@@ -26,17 +47,20 @@ const Board= () => {
                 type="text"
                 name="title"
                 placeholder="글 제목"
+                value={inputTitle}
+                onChange={onChange1}
             /> <br />
             <textarea 
                 name="content"
                 rows="5"
                 cols="20"
                 placeholder="내용"
+                value={inputContent}
+                onChange={onChange2}
             /> <br />
             <input type="file" name="file"/> <br/>
-            <button>작성하기</button> <br/>
-            <h4>{lists}</h4>
-
+            <button onClick={onClick}>작성하기</button> <br/>
+            {boardList} <br/>
         </div>
     )
 
